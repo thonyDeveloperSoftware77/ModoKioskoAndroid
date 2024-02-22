ca7b177394e6cbe0962cc358cd17609e20af3083
Anthony Cochea <thonydevelopersoftware@gmail.com>
2024-02-22 11:35:53 -0500

    Informacion de SIM

--- app/src/main/java/com/example/modokiosko/MainActivity.kt
+++ app/src/main/java/com/example/modokiosko/MainActivity.kt
@@ -4,7 +4,9 @@ import android.app.ActivityManager
 import android.app.admin.DevicePolicyManager
 import android.content.ComponentName
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.telephony.TelephonyManager
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -13,13 +15,21 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
+import android.Manifest
+import android.annotation.SuppressLint
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
 import com.example.modokiosko.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    companion object {
+        private val REQUEST_READ_PHONE_STATE = 1
+        private val REQUEST_READ_SMS = 2
+        private val REQUEST_READ_PHONE_NUMBERS = 3
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,11 +44,44 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            allowLockTaskModeToThisApp()
-            checkIfInKioskMode()
-            Snackbar.make(view, "Modo kiosko activado", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.READ_PHONE_STATE), REQUEST_READ_PHONE_STATE)
+            } else if (checkSelfPermission(Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.READ_SMS), REQUEST_READ_SMS)
+            } else if (checkSelfPermission(Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.READ_PHONE_NUMBERS), REQUEST_READ_PHONE_NUMBERS)
+            } else {
+                allowLockTaskModeToThisApp()
+                checkIfInKioskMode()
+                getSimInformation()
+                getPhoneNumber()
+                Snackbar.make(view, "Modo kiosko activado", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            }
         }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_READ_PHONE_STATE || requestCode == REQUEST_READ_SMS || requestCode == REQUEST_READ_PHONE_NUMBERS) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getPhoneNumber()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getPhoneNumber() {
+        val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        val phoneNumber = telephonyManager.line1Number
+        Log.d("PHONE_INFO", "Phone Number: $phoneNumber")
+    }
+    private fun getSimInformation() {
+        val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        val simState = telephonyManager.simState
+
+        Log.d("SIM_INFO", "SIM State: $simState")
     }
 
     private fun allowLockTaskModeToThisApp() {
@@ -46,11 +89,12 @@ class MainActivity : AppCompatActivity() {
         val deviceAdmin = ComponentName(this, DeviceAdminBroadcastReceiver::class.java)
         if (dpm.isDeviceOwnerApp(packageName)) {
             dpm.setLockTaskPackages(deviceAdmin, arrayOf(packageName))
-            Log.d("DEVICE_OWNER", "App is device owner")
+            Log.d("DEVICE_OWNERS", "App is device owners")
             // Iniciar el modo kiosko
-            startLockTask();
+            //startLockTask();
+            stopLockTask()
         } else {
-            Log.d("DEVICE_OWNER", "App is not device owner")
+            Log.d("DEVICE_OWNERS", "App is not device owners")
         }
     }
 
